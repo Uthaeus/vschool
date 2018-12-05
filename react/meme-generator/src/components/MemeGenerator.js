077914a7eebd28b5f382e5c11a130fb1ba005488
Uthaeus <romanlavery@gmail.com>
2018-12-05 09:56:33 -0700

    Importing images from api for meme generator

--- react/meme-generator/src/components/MemeGenerator.js
+++ react/meme-generator/src/components/MemeGenerator.js
@@ -1,7 +1,23 @@
 import React, { Component } from 'react';
 
 class MemeGenerator extends Component {
-    state = {}
+    state = {
+        topText: '',
+        bottomText: '',
+        randomImg: "http://i.imgflip.com/1bij.jpg",
+        allMemeImgs: []
+    }
+
+    componentDidMount() {
+        fetch("https://api.imgflip.com/get_memes")
+            .then(response => response.json())
+            .then(response => {
+                const {memes} = response.data
+                this.setState({
+                    allMemeImgs: memes    
+                });
+            });
+    }
 
     render() {
         return (
